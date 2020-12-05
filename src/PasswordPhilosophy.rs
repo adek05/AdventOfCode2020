b9c53797f55cfd61bff9bcf63dbd26f1cc0ca08b
Adrian Zgorzałek <adrian@adek.io>
2020-12-05 12:49:54 +0000

    [PasswordPhilosophy] Part 2.

--- src/PasswordPhilosophy.rs
+++ src/PasswordPhilosophy.rs
@@ -8,12 +8,30 @@ use std::io;
 use std::io::prelude::*;
 use std::path;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Input {
     min_cnt: usize,
     max_cnt: usize,
     letter: char,
     password: String,
+}
+
+struct InputPartTwo {
+    first_position: usize,
+    second_position: usize,
+    letter: char,
+    password: String,
+}
+
+impl From<Input> for InputPartTwo {
+    fn from(input: Input) -> InputPartTwo {
+        InputPartTwo {
+            first_position: input.min_cnt - 1,
+            second_position: input.max_cnt - 1,
+            letter: input.letter,
+            password: input.password,
+        }
+    }
 }
 
 // 1-3 a: abcde
@@ -36,7 +54,7 @@ fn read_input() -> Result<Vec<Input>, io::Error> {
     Ok(x)
 }
 
-fn is_password_valid(candidate: &Input) -> bool {
+fn is_password_valid_part_one(candidate: &Input) -> bool {
     let letter_count: usize = candidate
         .password
         .chars()
@@ -46,17 +64,34 @@ fn is_password_valid(candidate: &Input) -> bool {
     candidate.min_cnt <= letter_count && letter_count <= candidate.max_cnt
 }
 
+fn is_password_valid_part_two(candidate: &InputPartTwo) -> bool {
+    let password = &candidate.password;
+
+    (((password.chars().nth(candidate.first_position).unwrap() == candidate.letter) as u32)
+        + ((password.chars().nth(candidate.second_position).unwrap() == candidate.letter) as u32))
+        == 1
+}
+
 #[allow(dead_code)]
 fn main() {
     let input: Vec<Input> = read_input().unwrap();
 
-    let number_of_valid_passwords = input
+    let number_of_valid_passwords_part_1 = input
         .iter()
-        .filter(|input| is_password_valid(input))
+        .filter(|input| is_password_valid_part_one(input))
+        .count();
+    let number_of_valid_passwords_part_2 = input
+        .into_iter()
+        .filter(|input| is_password_valid_part_two(&InputPartTwo::from(input.clone())))
         .count();
 
     println!(
-        "Number of valid passwords is: {}",
-        number_of_valid_passwords
+        "Number of valid passwords in Part 1 is: {}",
+        number_of_valid_passwords_part_1
+    );
+
+    println!(
+        "Number of valid passwords in Part 2 is: {}",
+        number_of_valid_passwords_part_2
     );
 }
