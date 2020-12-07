ee2b475e916caf63cc808c8cd310c884b0c85237
Adrian Zgorzałek <adrian@adek.io>
2020-12-07 21:07:10 +0000

    [Handy Haversacks] Part 2.

--- src/handy_haversacks.rs
+++ src/handy_haversacks.rs
@@ -1,6 +1,8 @@
+extern crate itertools;
 #[macro_use]
 extern crate scan_rules;
 
+use itertools::izip;
 use scan_rules::scanner::Word;
 use std::collections::HashMap;
 use std::collections::HashSet;
@@ -16,20 +18,20 @@ struct Bag {
     color: String,
 }
 
+#[derive(Debug)]
 struct Graph {
-    can_be_contained: HashMap<Bag, Vec<Bag>>,
+    can_be_contained: HashMap<Bag, Vec<(Bag, i32)>>,
+    contains: HashMap<Bag, Vec<(Bag, i32)>>,
 }
 
-fn parse_line(line: String) -> Option<(Bag, Vec<Bag>)> {
-    if let Ok((bag, (adjectives, colors))) = scan!(
+fn parse_line(line: String) -> Option<(Bag, Vec<(Bag, i32)>)> {
+    if let Ok((bag, (count, adjectives, colors))) = scan!(
         &line;
-        ( let source_adj: Word<String>, let source_color: Word<String>, "bags contain", [ let _num: i32, let adj: Word<String>, let color: Word<String>, let _: Word ],+, "." ) => (Bag { adjective: source_adj, color: source_color }, (adj, color)),
-        ( let source_adj: Word<String>, let source_color: Word<String>, "bags contain no other bags.") => ( Bag { adjective: source_adj, color: source_color }, (vec![], vec![]))
+        ( let source_adj: Word<String>, let source_color: Word<String>, "bags contain", [ let count: i32, let adj: Word<String>, let color: Word<String>, let _: Word ],+, "." ) => (Bag { adjective: source_adj, color: source_color }, (count, adj, color)),
+        ( let source_adj: Word<String>, let source_color: Word<String>, "bags contain no other bags.") => ( Bag { adjective: source_adj, color: source_color }, (vec![], vec![], vec![]))
     ) {
-        let targets: Vec<Bag> = adjectives
-            .into_iter()
-            .zip(colors)
-            .map(|(adjective, color)| Bag { adjective, color })
+        let targets: Vec<(Bag, i32)> = izip!(count, adjectives, colors)
+            .map(|(count, adjective, color)| (Bag { adjective, color }, count))
             .collect();
         Some((bag, targets))
     } else {
@@ -42,18 +44,24 @@ fn read_input() -> Result<Graph, io::Error> {
         return Err(io::Error::from(io::ErrorKind::NotFound));
     }
     let file = File::open("in/HandyHaversacks.in")?;
-    let mut graph: HashMap<Bag, Vec<Bag>> = HashMap::new();
+    let mut graph: HashMap<Bag, Vec<(Bag, i32)>> = HashMap::new();
+    let mut inverted_graph: HashMap<Bag, Vec<(Bag, i32)>> = HashMap::new();
 
     io::BufReader::new(file).lines().for_each(|line| {
         if let Some((source, targets)) = parse_line(line.unwrap()) {
-            targets
-                .into_iter()
-                .for_each(|target| graph.entry(target).or_default().push(source.clone()));
+            targets.clone().into_iter().for_each(|(target, count)| {
+                graph
+                    .entry(target)
+                    .or_default()
+                    .push((source.clone(), count))
+            });
+            inverted_graph.entry(source).or_default().extend(targets);
         }
     });
 
     Ok(Graph {
         can_be_contained: graph,
+        contains: inverted_graph,
     })
 }
 
@@ -65,24 +73,45 @@ fn main() {
     };
 
     if let Ok(graph) = read_input() {
-        let mut queue: VecDeque<&Bag> = VecDeque::new();
-        queue.push_back(&my_bag);
+        {
+            let mut queue: VecDeque<&Bag> = VecDeque::new();
+            queue.push_back(&my_bag);
 
-        let mut result: HashSet<&Bag> = HashSet::new();
-        while let Some(bag) = queue.pop_front() {
-            if result.contains(bag) {
-                continue;
+            let mut result: HashSet<&Bag> = HashSet::new();
+            while let Some(bag) = queue.pop_front() {
+                if result.contains(bag) {
+                    continue;
+                }
+                result.insert(bag);
+                if let Some(bags) = graph.can_be_contained.get(bag) {
+                    queue.extend(bags.iter().map(|(bag, _)| bag));
+                }
             }
-            result.insert(bag);
-            if let Some(bags) = graph.can_be_contained.get(bag) {
-                queue.extend(bags);
-            }
+            result.remove(&my_bag);
+
+            println!(
+                "Part 1. Number of different outer bags is: {}",
+                result.len()
+            );
         }
-        result.remove(&my_bag);
 
-        println!(
-            "Part 1. Number of different outer bags is: {}",
-            result.len()
-        );
+        {
+            let mut queue: VecDeque<(&Bag, i32)> = VecDeque::new();
+            queue.push_back((&my_bag, 1));
+
+            let mut total_contained_bags = 0;
+
+            while let Some((bag, count)) = queue.pop_front() {
+                total_contained_bags += count;
+                if let Some(bags) = graph.contains.get(bag) {
+                    queue.extend(bags.iter().map(|(bag, how_many)| (bag, count * how_many)));
+                } 
+            }
+
+            println!(
+                "Part 2. Total of bags to buy: {}",
+                total_contained_bags - 1,
+            );
+        }
     }
 }
