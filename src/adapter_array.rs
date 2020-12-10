4413df2e5b7c3330d859b9c12c6b596c37516b5b
Adrian Zgorzałek <adrian@adek.io>
2020-12-10 13:52:34 +0000

    [AdapterArray] Part 2

--- src/adapter_array.rs
+++ src/adapter_array.rs
@@ -34,11 +34,27 @@ fn histogram(input: &[i64]) -> (i64, i64) {
     (ones, threes)
 }
 
+fn count_arrangements(input: &[i64]) -> i64 {
+    let mut dp: Vec<i64> = Vec::new();
+    dp.resize(input.len(), 0);
+    dp[0] = 1;
+    for i in 1..input.len() {
+        let mut j: i64 = (i - 1) as i64;
+        while (j >= 0) && (input[i] - input[j as usize] <= 3) {
+            dp[i] += dp[j as usize];
+            j -= 1;
+        }
+    }
+    *dp.last().unwrap()
+}
+
 fn main() {
     if let Ok(mut input) = read_input() {
         input.push(0);
         input.sort();
         let (ones, threes) = histogram(&input);
         println!("Part 1. #1s * #3s = {}", ones * (threes + 1));
+
+        println!("Part 2. Number of different arrangments is {}", count_arrangements(&input));
     }
 }
