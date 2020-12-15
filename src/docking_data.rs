14059157853ed36955887e16a901ac8da81483a3
Adrian Zgorzałek <adrian@adek.io>
2020-12-15 20:32:03 +0000

    [DockingData] Part 2.

--- src/docking_data.rs
+++ src/docking_data.rs
@@ -13,12 +13,14 @@ struct Bitmask(String);
 
 struct Program {
     bitmask: Bitmask,
-    memory: HashMap<u64, String>,
+    memory: HashMap<u64, u64>,
 }
 
+#[derive(Clone)]
 enum Operation {
     SetBitmask(Bitmask),
-    SetValue(u64, u64), // Memory Location -> Value
+    SetValue(u64, u64),      // Memory Location -> Value
+    BatchSetValue(u64, u64), // Memory Location -> Value
 }
 
 fn execute_operation(p: Program, op: &Operation) -> Program {
@@ -29,10 +31,19 @@ fn execute_operation(p: Program, op: &Operation) -> Program {
         },
         Operation::SetValue(mem_location, value) => {
             let mut memory = p.memory;
-            memory.insert(
-                *mem_location,
-                format!("{:037b}", apply_bitmask(*value, &p.bitmask)),
-            );
+            memory.insert(*mem_location, apply_bitmask(*value, &p.bitmask));
+            Program {
+                bitmask: p.bitmask,
+                memory,
+            }
+        }
+        Operation::BatchSetValue(mem_location, value) => {
+            let mut memory = p.memory;
+            apply_bitmask_2(format!("{:b}", mem_location), &p.bitmask)
+                .iter()
+                .for_each(|address| {
+                    memory.insert(u64::from_str_radix(address, 2).unwrap(), *value);
+                });
             Program {
                 bitmask: p.bitmask,
                 memory,
@@ -50,6 +61,27 @@ fn apply_bitmask(mut value: u64, bitmask: &Bitmask) -> u64 {
         }
     }
     value
+}
+
+fn apply_bitmask_2(address: String, bitmask: &Bitmask) -> Vec<String> {
+    let mut addresses = vec!["".to_string()];
+    let padded_address = format!("{:0>36}", address);
+    assert_eq!(padded_address.len(), bitmask.0.len());
+    bitmask
+        .0
+        .chars()
+        .zip(padded_address.chars())
+        .for_each(|(mask, c)| match mask {
+            '0' => addresses = addresses.iter().map(|s| format!("{}{}", s, c)).collect(),
+            '1' => addresses = addresses.iter().map(|s| format!("{}1", s)).collect(),
+            _ => {
+                addresses = addresses
+                    .iter()
+                    .flat_map(|s| vec![format!("{}1", s), format!("{}0", s)])
+                    .collect()
+            }
+        });
+    addresses
 }
 
 fn read_input() -> Result<Vec<Operation>, String> {
@@ -84,7 +116,28 @@ fn main() {
                 program_end
                     .memory
                     .values()
-                    .map(|value| u64::from_str_radix(value, 2).unwrap())
+                    // .map(|value| u64::from_str_radix(value, 2).unwrap())
+                    .sum::<u64>(),
+            );
+
+            let program_end_2 = operations
+                .iter()
+                .map(|op| match op {
+                    Operation::SetValue(location, value) => Operation::BatchSetValue(*location, *value),
+                    x => x.clone(),
+                })
+                .fold(
+                    Program {
+                        bitmask: Bitmask("".to_string()),
+                        memory: HashMap::new(),
+                    },
+                    |acc, op| execute_operation(acc, &op),
+                );
+            println!(
+                "Part 2. Sum of all memory locations set is: {}",
+                program_end_2
+                    .memory
+                    .values()
                     .sum::<u64>(),
             );
         }
