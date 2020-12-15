152632daf05d0faaf95b3865a39a7e3f836b3143
Adrian Zgorzałek <adrian@adek.io>
2020-12-15 15:19:48 +0000

    [ShuttleSearch] Part 2

--- src/shuttle_search.rs
+++ src/shuttle_search.rs
@@ -3,34 +3,54 @@ use std::io;
 use std::io::prelude::*;
 use std::path;
 
-fn read_input() -> Result<(u64, Vec<u64>), String> {
+fn read_input() -> Result<(u64, Vec<(u64, u64)>), String> {
     if !path::Path::new("in").exists() {
         return Err("File not found".to_string());
     }
     let file = File::open("in/ShuttleSearch.in").map_err(|_| "Input file not found".to_string())?;
     let mut line_iter = io::BufReader::new(file).lines();
     let time_at_bus_stop = line_iter.next().unwrap().unwrap().parse::<u64>().unwrap();
-    let buses: Vec<u64> = line_iter
+    let buses: Vec<(u64, u64)> = line_iter
         .next()
         .unwrap()
         .unwrap()
         .split(',')
-        .filter_map(|value| {
+        .enumerate()
+        .filter_map(|(idx, value)| {
             if value == "x" {
                 None
             } else {
-                Some(value.parse::<u64>().unwrap())
+                Some((idx as u64, value.parse::<u64>().unwrap()))
             }
         })
         .collect();
     Ok((time_at_bus_stop, buses))
 }
 
+fn find_time(buses: &[(u64, u64)]) -> u64 {
+    let mut lcm = 1;
+    let mut first_matching = 0;
+    for (mut modulus, bus_id) in buses {
+        modulus %= bus_id;
+        let mut cur = first_matching;
+        while (cur + modulus) % bus_id != 0 {
+            cur += lcm;
+        }
+        first_matching = cur;
+        lcm *= *bus_id; // Should be real LCM, but all input numbers are prime.
+        println!(
+            "Looking for bus_id {} modulus {}. Current lcm = {} and first_matching = {}",
+            bus_id, modulus, lcm, first_matching
+        );
+    }
+    first_matching
+}
+
 fn main() {
     if let Ok((earliest_time, bus_ids)) = read_input() {
-        let mut ts: Vec<(u64, u64, u64)> = bus_ids
+        let mut departure_times: Vec<(u64, u64, u64)> = bus_ids
             .iter()
-            .map(|bus_id| {
+            .map(|(_, bus_id)| {
                 let m = earliest_time % bus_id;
                 if m == 0 {
                     (earliest_time, *bus_id, 0)
@@ -40,12 +60,15 @@ fn main() {
                 }
             })
             .collect();
-        ts.sort_by(|a, b| a.0.cmp(&b.0));
-        if let Some((_, bus_id, wait_time)) = ts.first() {
+        departure_times.sort_by(|a, b| a.0.cmp(&b.0));
+        if let Some((_, bus_id, wait_time)) = departure_times.first() {
             println!(
                 "Part 1. Earliest bus_id * wait_time = {}",
                 bus_id * wait_time
             );
         }
+
+        let time = find_time(&bus_ids);
+        println!("Part 2. Earliest time for competition: {}", time);
     }
 }
