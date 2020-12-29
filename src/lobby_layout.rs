d556be085d4ae2316d86772e69974f20a6619c7e
Adrian Zgorzałek <adrian@adek.io>
2020-12-29 20:24:31 +0000

    [LobbyLayout] Part 2

--- src/lobby_layout.rs
+++ src/lobby_layout.rs
@@ -1,12 +1,12 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::path;
-use std::collections::HashSet;
 
 type Coord = (i64, i64);
 
-fn apply_moves(mut start: Coord, moves: String) -> Coord {
+fn apply_moves(mut start: Coord, moves: &str) -> Coord {
     let mut it = moves.chars();
     while let Some(c) = it.next() {
         match c {
@@ -43,22 +43,85 @@ fn read_input() -> Result<Vec<String>, String> {
     }
     let file =
         File::open("in/LobbyLayout.in").map_err(|_| "Input file not found".to_string())?;
-    Ok(io::BufReader::new(file).lines().map(|line| line.unwrap()).collect())
+    Ok(io::BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap())
+        .collect())
+}
+
+fn neighbors(tile: Coord) -> [Coord; 6] {
+    [
+        apply_moves(tile, "ne"),
+        apply_moves(tile, "nw"),
+        apply_moves(tile, "se"),
+        apply_moves(tile, "sw"),
+        apply_moves(tile, "e"),
+        apply_moves(tile, "w"),
+    ]
 }
 
 fn main() {
-    assert_eq!((0, 0), apply_moves((0, 0), "nwwswee".to_string()));
-    assert_eq!((1, -1), apply_moves((0, 0), "esew".to_string()));
+    assert_eq!((0, 0), apply_moves((0, 0), "nwwswee"));
+    assert_eq!((1, -1), apply_moves((0, 0), "esew"));
+
     if let Ok(instructions) = read_input() {
-        let mut touched_tiles: HashSet<Coord> = HashSet::new();
+        let mut black_tiles: HashSet<Coord> = HashSet::new();
         for instruction in instructions {
-            let result = apply_moves((0, 0), instruction);
-            if touched_tiles.contains(&result) {
-                touched_tiles.remove(&result);
+            let result = apply_moves((0, 0), &instruction);
+            if black_tiles.contains(&result) {
+                black_tiles.remove(&result);
             } else {
-                touched_tiles.insert(result);
+                black_tiles.insert(result);
             }
         }
-        println!("Part 1. Number of flipped tiles is: {}", touched_tiles.len());
+        println!(
+            "Part 1. Number of flipped tiles is: {}",
+            black_tiles.len()
+        );
+
+        for _ in 0..100 {
+            let white_candidates: HashSet<Coord> = black_tiles
+                .iter()
+                .flat_map(|tile| neighbors(*tile).to_vec())
+                .collect::<HashSet<Coord>>()
+                .difference(&black_tiles)
+                .cloned()
+                .collect();
+
+            let mut new_black_tiles: HashSet<Coord> = black_tiles
+                .iter()
+                .filter(|black_tile| {
+                    let n_black_neighbords = neighbors(**black_tile)
+                        .iter()
+                        .cloned()
+                        .collect::<HashSet<Coord>>()
+                        .intersection(&black_tiles)
+                        .count();
+                    n_black_neighbords == 1 || n_black_neighbords == 2
+                })
+                .cloned()
+                .collect();
+
+            let black_tiles_from_white_tiles: HashSet<Coord> = white_candidates
+                .iter()
+                .filter(|white_tile| {
+                    neighbors(**white_tile)
+                        .iter()
+                        .cloned()
+                        .collect::<HashSet<Coord>>()
+                        .intersection(&black_tiles)
+                        .count()
+                        == 2
+                })
+                .cloned()
+                .collect();
+
+            new_black_tiles.extend(&black_tiles_from_white_tiles);
+            black_tiles = new_black_tiles;
+        }
+        println!(
+            "Part 2. Number of black tiles after 100 days is {}",
+            black_tiles.len()
+        );
     }
 }
